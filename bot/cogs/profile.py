02a875856523dacbb67f114544d45d0f4c6f2986
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-05-03 07:39:38 +0530

    Add pyprofiles

--- bot/cogs/profile.py
+++ bot/cogs/profile.py
@@ -1,13 +1,13 @@
 from discord.ext import commands
-
+from profiles.quora import User
 
 class Profile(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 	
 	@commands.command()
-	async def profile(self, ctx):
-		await ctx.send("Hmm")
+	async def profile(self, ctx, arg):
+		await ctx.send(User(arg))
 
 def setup(bot):
     x = Profile(bot)
