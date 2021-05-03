7945f156b776dafb433f0758210f207f5cb72621
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-05-03 16:32:59 +0530

    Add utility functions

---
+++ bot/utils/parser.py
@@ -0,0 +1,15 @@
+import re
+
+
+def extract_links(text):
+    pattern = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    return [i[o] for i in re.findall(pattern, text)]
+
+
+def extract_quora_username(text):
+    pattern = r"(http://|https://|)(www.|)(quora.com/profile/|)([A-Za-z0-9\-]+)/?(.*)"
+    match = re.match(pattern, text)
+    if match is None:
+        return None
+    else:
+        return match.group(4)
