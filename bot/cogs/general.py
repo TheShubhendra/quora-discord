5c24fbdeecb5ec3aba87fc10dc40707282d26b2a
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-05-03 11:49:11 +0530

    Add general cogs

---
+++ bot/cogs/general.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+
+INVITE_LINK = "https://discord.com/api/oauth2/authorize?client_id=838250557805821992&permissions=2147765312&scope=bot"
+
+
+class General(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def invite(self, ctx):
+        await ctx.send(INVITE_LINK)
+
+
+def setup(bot):
+    x = General(bot)
+    bot.add_cog(x)
