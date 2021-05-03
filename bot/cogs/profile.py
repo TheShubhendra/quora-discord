b4f49efcada2f093c4121fb5d0fbd09d7b3b2212
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-05-03 11:48:55 +0530

    Fix code style

--- bot/cogs/profile.py
+++ bot/cogs/profile.py
@@ -1,13 +1,15 @@
 from discord.ext import commands
 from profiles.quora import User
 
+
 class Profile(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
-	
-	@commands.command()
-	async def profile(self, ctx, arg):
-		await ctx.send(User(arg))
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def profile(self, ctx, arg):
+        await ctx.send(User(arg))
+
 
 def setup(bot):
     x = Profile(bot)
