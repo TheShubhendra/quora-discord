2008aa0eb9a9a8c4e52e68ddd68706806c585abf
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-05-04 15:16:27 +0530

    Add activity

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord import Streaming
 from decouple import config
 import logging
 import glob
@@ -7,8 +8,18 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(message)s",
     level=logging.INFO,
 )
+
 TOKEN = config("TOKEN")
-bot = commands.Bot(command_prefix="q!")
+
+activity = Streaming(
+    name="Quora",
+    url="https://quora.com",
+)
+bot = commands.Bot(
+    command_prefix="q!",
+    description="Type q!help to get help.",
+    activity=activity,
+)
 
 for cog in glob.glob("bot/cogs/*.py"):
     bot.load_extension(cog[:-3].replace("/", "."))
