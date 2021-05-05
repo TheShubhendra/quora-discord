c99ea97edd0f25f169e42854af8ec9819cef52c5
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-05-05 15:25:55 +0530

    Fix Embed

--- bot/utils/embeds.py
+++ bot/utils/embeds.py
@@ -4,24 +4,37 @@ from discord import Embed, Colour
 def profile_embed(profile):
     embed = Embed(
         title=profile.username,
-        colour = Colour.random(),
+        colour=Colour.random(),
     )
-    #for i,j in vars(profile).items():
-        #embed.add_field(name=i, value=j)
+    # for i,j in vars(profile).items():
+    # embed.add_field(name=i, value=j)
     embed.set_thumbnail(url=str(profile.profileImage))
-    embed.add_field(name = "First Name", value = profile.firstName, inline=True)
-    embed.add_field(name = "Last Name", value = profile.lastName, inline=True)
-    embed.add_field(name = "Credential", value = profile.profileCrendential)
-    embed.add_field(name = "Contributing Space", value = profile.contributingSpaceCount)
-    embed.add_field(name = "Answer Views", value = profile.answerViewsCount)
-    embed.add_field(name = "Content Views", value = profile.contentViewsCount)
-    embed.add_field(name = "Last month content views", value = profile.lastMonthContentView)
-    embed.add_field(name = "Answer Count", value = profile.answerCount)
-    embed.add_field(name = "Question Count", value = profile.questionCount)
-    embed.add_field(name = "Post Count", value = profile.postCount)
-    embed.add_field(name = "Share Count", value = profile.shareCount)
-    embed.add_field(name = "Followers", value = profile.followerCount)
-    embed.add_field(name = "Following", value = profile.followingCount)
-    embed.add_field(name = "Following Space", value = profile.followingSpaceCount)
-    embed.add_field(name = "Following Topics", value = profile.followingTopicCount)
+    embed.add_field(
+        name="General",
+        value="**First Name:** {0.firstName}\n**Last Name:** {0.lastName}\n**Crendential:** {0.profileCrendential}\n**Contributing Space:** {0.contributingSpaceCount}".format(
+            profile
+        ),
+    )
+
+    embed.add_field(
+        name="Views",
+        value="**Answer Views:** {0.answerViewsCount}\n**Content Views:** {0.contentViewsCount}\n**Last Month Content View:** {0.lastMonthContentView}".format(
+            profile
+        ),
+    )
+
+    embed.add_field(
+        name="Counts",
+        value="**Answer Count:** {0.answerCount}\n**Question Count:** {0.questionCount}\n**Post Count:** {0.postCount}\n**Share Count:** {0.shareCount}".format(
+            profile
+        ),
+    )
+
+    embed.add_field(
+        name="Follow Counts",
+        value="**Follower:** {0.followerCount}\n**Following Count:** {0.followingCount}\n**Following Space:** {0.followingSpaceCount}\n**Following Topics:** {0.followingTopicCount}".format(
+            profile
+        ),
+    )
+
     return embed
