f7f0d5b84e273b5a29a0070d9d5a60b4707bb730
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-08-05 18:01:24 +0530

    Add handler for follower change

--- bot/modules/whandler.py
+++ bot/modules/whandler.py
@@ -11,11 +11,28 @@ async def inform_answer_count_change(event):
         for dest in destinations:
             channel = await bot.fetch_channel(dest["channel_id"])
             await channel.send(
-                f"Congratulations!!  <@{dest['discord_id']}> for writing {event.countChange} answer  on Quora."
+                f"Congratulations!!  <@{dest['discord_id']}> for writing {event.countChange} answer  on Quora.\nCurrent Answer count: {event.newCount}"
             )
     else:
         for dest in destinations:
             channel = await bot.fetch_channel(dest["channel_id"])
             await channel.send(
-                f"Hii!!  <@{dest['discord_id']}> {abs(event.countChange)} answer(s) is/are not visible on your profile. If you haven't deleted them, it may have collapsed or deleted by Quora."
+                f"Hii!!  <@{dest['discord_id']}> {abs(event.countChange)} answer(s) is/are not visible on your profile. If you haven't deleted them, it may have collapsed or deleted by Quora.\nCurrent Asnwer Count: {event.newCount}"
+            )
+
+
+@bot.watcher.dispatcher.on(FollowerCountChange)
+async def inform_follower_count_change(event):
+    destinations = event.data_dict["dispatch_to"]
+    if event.countChange >= 1:
+        for dest in destinations:
+            channel = await bot.fetch_channel(dest["channel_id"])
+            await channel.send(
+                f"Congratulations!!  <@{dest['discord_id']}> , you got {event.countChange} new follower(s) on Quora.\nCurrent follower count: {event.newCount}"
+            )
+    else:
+        for dest in destinations:
+            channel = await bot.fetch_channel(dest["channel_id"])
+            await channel.send(
+                f"Hii!! <@{dest['discord_id']}> {abs(event.countChange)} people unfollowed you on Quora.\nCurrent Follower Count: {event.newCount}"
             )
