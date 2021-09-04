937ef6c2f6e72203f0e1343ee7076e4852d1731f
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-09-04 15:31:43 +0530

    Alter Database schema Use Foreign keys, create seperate tables for quora profiles

--- bot/database/tables.py
+++ bot/database/tables.py
@@ -1,71 +1,55 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import (
+    BigInteger,
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+)
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
 class Guild(Base):
-    __tablename__ = "guild_data"
-    guild_id = Column(String(50), primary_key=True)
-    guild_name = Column(String(50))
-    update_channel = Column(String(50))
-
-    def __init__(
-        self,
-        guild_id,
-        guild_name,
-        update_channel,
-    ):
-        self.guild_id = guild_id
-        self.guild_name = guild_name
-        self.update_channel = update_channel
+    __tablename__ = "guilds"
+    id = Column(BigInteger, primary_key=True)
+    name = Column(String(50))
+    update_channel = Column(BigInteger)
+    watchers = relationship("Watcher", backref="guild")
 
 
 class Watcher(Base):
-    __tablename__ = "watcher_data"
+    __tablename__ = "watchers"
     id = Column(Integer, primary_key=True)
-    guild_id = Column(String(50))
-    user_id = Column(Integer)
-
-    def __init__(
-        self,
-        guild_id,
-        user_id,
-    ):
-        self.guild_id = guild_id
-        self.user_id = user_id
+    guild_id = Column(BigInteger, ForeignKey("guilds.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
 
 
-class Quoran(Base):
-    __tablename__ = "quoran"
-    user_id = Column(Integer, primary_key=True)
-    discord_id = Column(String(50))
+class Profile(Base):
+    __tablename__ = "profiles"
+    id = Column(Integer, primary_key=True)
+    language = Column(String(10))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    answer_count = Column(Integer, default=0)
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    discord_id = Column(BigInteger)
     discord_username = Column(String(100))
-    quora_username = Column(String(50))
-    follower_count = Column(Integer)
-    answer_count = Column(Integer)
-    access = Column(String(10))
-
-    def __init__(
-        self,
-        discord_id,
-        discord_username,
-        quora_username,
-        answer_count=0,
-        follower_count=0,
-        access="public",
-    ):
-        self.discord_id = discord_id
-        self.discord_username = discord_username
-        self.quora_username = quora_username
-        self.follower_count = follower_count
-        self.answer_count = answer_count
-        self.access = access
+    quora_username = Column(String(100))
+    follower_count = Column(Integer, default=0)
+    access = Column(String(10), default="public")
+    watchers = relationship("Watcher", backref="user")
+    profiles = relationship("Profile", backref="user")
 
 
 __all__ = [
     Base,
+    Profile,
     Guild,
+    User,
     Watcher,
-    Quoran,
 ]
